b37d3d0ef106d8065191e2784335c9e6d0ee4563
Joseph Ani <joeoani@gmail.com>
2023-03-03 13:46:27 -0500

    first commit

---
+++ data/products.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 'price_1MhFXIGjvNpKCuOSS5izQlub',
+    name: 'Watermelon',
+    price: '1.00',
+    emoji: '🍉',
+    currency: 'USD',
+  },
+  {
+    id: 'price_1MhFf5GjvNpKCuOSRx4gRdNU',
+    name: 'Apple',
+    price: '1.00',
+    emoji: '🍎',
+    currency: 'USD',
+  },
+  {
+    id: 'price_1MhFgKGjvNpKCuOS0QSCqgZj',
+    name: 'Pineapple',
+    price: '2.00',
+    emoji: '🍍',
+    currency: 'USD',
+  },
+  {
+    id: 'price_1MhFgzGjvNpKCuOSNFepNGpE',
+    name: 'Banana',
+    price: '1.00',
+    emoji: '🍌',
+    currency: 'USD',
+  },
+]
