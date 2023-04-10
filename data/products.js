1bce1799608961eeef3a7804d93dccd1d326c313
Joseph Ani <joeoani@gmail.com>
2023-04-10 12:45:43 -0400

    shoppingcart

--- data/products.js
+++ data/products.js
@@ -2,28 +2,32 @@ export const products = [
   {
     id: 'price_1MhFXIGjvNpKCuOSS5izQlub',
     name: 'Watermelon',
-    price: '1.00',
+    /* Prices are in smallest currency unit, 100 cents = $1 */
+    price: '100',
     emoji: '🍉',
     currency: 'USD',
   },
   {
     id: 'price_1MhFf5GjvNpKCuOSRx4gRdNU',
     name: 'Apple',
-    price: '1.00',
+    /* Prices are in smallest currency unit, 100 cents = $1 */
+    price: '100',
     emoji: '🍎',
     currency: 'USD',
   },
   {
     id: 'price_1MhFgKGjvNpKCuOS0QSCqgZj',
     name: 'Pineapple',
-    price: '2.00',
+    /* Prices are in smallest currency unit, 20 cents = $0.20 */
+    price: '200',
     emoji: '🍍',
     currency: 'USD',
   },
   {
     id: 'price_1MhFgzGjvNpKCuOSNFepNGpE',
     name: 'Banana',
-    price: '1.00',
+    /* Prices are in smallest currency unit, 100 cents = $1 */
+    price: '100',
     emoji: '🍌',
     currency: 'USD',
   },
